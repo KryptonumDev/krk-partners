b52670bb0174ad16965fb598a4fb243e7697b657
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-02-23 09:22:24 +0100

    added Checbox animation

--- next/src/components/ui/Checkbox/Checkbox.tsx
+++ next/src/components/ui/Checkbox/Checkbox.tsx
@@ -30,13 +30,16 @@ export default Checkbox;
 const Checkmark = () => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width={12}
+    width={14}
     height={10}
     fill='none'
   >
     <path
-      d='M11.373.96a1 1 0 0 1 0 1.414L4.707 9.04a1 1 0 0 1-1.414 0L.626 6.374A1 1 0 1 1 2.04 4.959L4 6.92 9.96.959a1 1 0 0 1 1.413 0Z'
-      fill='#15A669'
+      d='M1.5 5.5 4.999 9l7.5-8'
+      stroke='#15A669'
+      strokeWidth={2}
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
   </svg>
 );
