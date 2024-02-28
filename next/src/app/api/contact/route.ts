b8e153e5d62aade4d5ecdeeecd13cc803ebb4afa
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-02-28 14:47:34 +0100

    added /api/contact serverless route

---
+++ next/src/app/api/contact/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+import { regex } from '@/global/constants';
+import { removeHtmlTags } from '@/utils/remove-html-tags';
+
+type RequestProps = {
+  message: string;
+  email: string;
+  legal: string;
+};
+
+const resend = new Resend(process.env.RESEND_API_TOKEN);
+
+const emailData = {
+  from: 'biuro@krk-partners.pl',
+  to: 'bogumil@kryptonum.eu', // TODO: change emailData.to
+};
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  const { message, email, legal }: RequestProps = req;
+
+  if (!message || (!email && !regex.email.test(email)) || !legal) {
+    return NextResponse.json({ success: false }, { status: 422 });
+  }
+
+  const body = `<p>Adres e-mail: <b>${email}</b></p>
+  <p>${message.trim()}</p>
+  <br />
+  <br />
+  <p><em>Wyrażono zgodnę na politykę prywatności</em></p>
+  `;
+
+  try {
+    await resend.emails.send({
+      from: emailData.from,
+      reply_to: email,
+      to: emailData.to,
+      subject: `${email} wysłał wiadomość przez formularz kontaktowy`,
+      html: body,
+      text: removeHtmlTags(body),
+    });
+    return NextResponse.json({ success: true });
+  } catch {
+    return NextResponse.json({ success: false }, { status: 422 });
+  }
+}
