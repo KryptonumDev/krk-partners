77f9c39146f0abdc4928802d2d740913aa349193
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-04 12:50:23 +0100

    added addWorkdaysToDate func to Steps component

--- next/src/components/_global/Steps/Steps.tsx
+++ next/src/components/_global/Steps/Steps.tsx
@@ -5,6 +5,25 @@ import styles from './Steps.module.scss';
 import Timeline from './_Timeline';
 import type { Props } from './Steps.types';
 
+function addWorkdaysToDate(numWorkdays: number): string {
+  let currentDate: Date = new Date(new Date().getTime());
+  const weekdayNames: string[] = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
+  function isWeekend(date: Date): boolean {
+    return date.getDay() === 0 || date.getDay() === 6;
+  }
+  for (let i: number = 0; i < numWorkdays; ) {
+    currentDate.setDate(currentDate.getDate() + 1);
+    if (!isWeekend(currentDate)) {
+      i++;
+    }
+  }
+  let formattedDate: string = `${weekdayNames[currentDate.getDay()]} (${currentDate.getDate()}.${
+    currentDate.getMonth() + 1 < 10 ? '0' : ''
+  }${currentDate.getMonth() + 1})`;
+
+  return formattedDate;
+}
+
 const Steps = ({ heading, timeline, cta_Heading, cta_Paragraph, cta_Cta, cta_Img }: Props) => {
   return (
     <section className={styles['Steps']}>
@@ -14,7 +33,7 @@ const Steps = ({ heading, timeline, cta_Heading, cta_Paragraph, cta_Cta, cta_Img
       <Timeline timeline={timeline} />
       <div className={styles.ctaBox}>
         <Markdown.h3>{cta_Heading}</Markdown.h3>
-        <Markdown className={styles.paragraph}>{cta_Paragraph}</Markdown>
+        <Markdown className={styles.paragraph}>{cta_Paragraph.replace('${date}', addWorkdaysToDate(6))}</Markdown>
         <Button data={cta_Cta} />
         <div className={styles.img}>
           <Img
