d0416d7e8ec58ec0c9b1171f70121aa3a619f26a
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-04 14:29:53 +0100

    removed CookieConsent

--- next/src/components/_global/CookieConsent/index.ts
+++
@@ -1,9 +0,0 @@
-import CookieConsent from './CookieConsent';
-export type { Props as CookieConsentProps } from './CookieConsent.types';
-export default CookieConsent;
-
-export const CookieConsent_Query = `
-  _type == "CookieConsent" => {
-    _type,
-  },
-`;
