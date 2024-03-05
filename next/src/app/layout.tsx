3139833827073842576a56c128f9ba2a18e2f7c2
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-05 11:43:46 +0100

    updated isProduction check in layout.tsx

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -9,7 +9,7 @@ export const viewport: Viewport = {
   themeColor: themeColor,
 };
 
-const isProduction = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
