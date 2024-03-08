021fa748d9fe463225d288ae79db5661e9d3223e
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-08 17:11:58 +0100

    added authorizationHeader for revalidate in api route

--- next/src/app/api/revalidate/route.ts
+++ next/src/app/api/revalidate/route.ts
@@ -2,6 +2,12 @@ import { NextRequest } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
 export async function POST(request: NextRequest) {
+  const authorizationHeader = request.headers.get('Authorization');
+
+  if (authorizationHeader !== `Bearer ${process.env.SANITY_REVALIDATE_TOKEN}`) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+
   const tag = request.nextUrl.searchParams.get('tag');
   if (tag) {
     revalidateTag(tag);
