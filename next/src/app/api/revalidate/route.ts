b41b4e35605c503422c704a57db34ad8b8c002fe
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-08 16:27:57 +0100

    added revalidation api route

---
+++ next/src/app/api/revalidate/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from 'next/server';
+import { revalidateTag } from 'next/cache';
+
+export async function POST(request: NextRequest) {
+  const tag = request.nextUrl.searchParams.get('tag');
+  if (tag) {
+    revalidateTag(tag);
+    return Response.json({ revalidated: true, now: Date.now() });
+  } else {
+    return Response.json({ revalidated: false, now: Date.now() });
+  }
+}
