2255e0cda9f94d20a54e879d985ac6a9e090da0b
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-08 18:16:28 +0100

    added test console.log for revalidate route

--- next/src/app/api/revalidate/route.ts
+++ next/src/app/api/revalidate/route.ts
@@ -7,18 +7,19 @@ type RequestType = {
 
 export async function POST(request: NextRequest) {
   const authorizationHeader = request.headers.get('Authorization');
-  const { tag } = (await request.json()) as RequestType;
+  const test = (await request.json()) as RequestType;
 
-  console.log(tag);
+  console.log(test);
+  return;
 
-  if (authorizationHeader !== `Bearer ${process.env.SANITY_REVALIDATE_TOKEN}`) {
-    return new Response('Unauthorized', { status: 401 });
-  }
+  // if (authorizationHeader !== `Bearer ${process.env.SANITY_REVALIDATE_TOKEN}`) {
+  //   return new Response('Unauthorized', { status: 401 });
+  // }
 
-  if (tag) {
-    revalidateTag(tag);
-    return Response.json({ revalidated: true, now: Date.now() });
-  } else {
-    return Response.json({ revalidated: false, now: Date.now() });
-  }
+  // if (tag) {
+  //   revalidateTag(tag);
+  //   return Response.json({ revalidated: true, now: Date.now() });
+  // } else {
+  //   return Response.json({ revalidated: false, now: Date.now() });
+  // }
 }
