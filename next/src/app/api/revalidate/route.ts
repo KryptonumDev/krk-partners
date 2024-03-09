57902fac9c1676a026affff1c9040254a8d037ba
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-09 11:27:05 +0100

    fixed typo in queryHeader in revalidate route

--- next/src/app/api/revalidate/route.ts
+++ next/src/app/api/revalidate/route.ts
@@ -33,8 +33,7 @@ export async function POST(request: NextRequest) {
 
 const query = async (tag: string, id?: string): Promise<QueryType> => {
   let queryHeader = `*[_type == "${tag}"]`;
-  if (id) queryHeader = `*[_type == "${tag}"] && _id == "${id}"`;
-  console.log(queryHeader);
+  if (id) queryHeader = `*[_type == "${tag}"] && _id == "${id}"]`;
   return await sanityFetch<QueryType>({
     query: /* groq */ `
       ${queryHeader}{
