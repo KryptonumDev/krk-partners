d8e1e5b359bc29cb9d0b9491188560d1a1726292
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-04-23 10:14:26 +0200

    added GoogleTagManager from next/third-parties

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Script from 'next/script';
+import { GoogleTagManager } from '@next/third-parties/google';
 import '@/global/global.scss';
 import { locale, themeColor } from '@/global/constants';
 import { lato, cinzel } from '@/global/fonts';
@@ -14,28 +14,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang={locale}>
-      <head>
-        {isProduction && (
-          <Script id='gtm'>
-            {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T3N838XB');
-          `}
-          </Script>
-        )}
-      </head>
+      {isProduction && <GoogleTagManager gtmId="GTM-T3N838XB" />}
       <body className={`${lato.className} ${cinzel.variable}`}>
-        {isProduction && (
-          <noscript>
-            <iframe
-              src='https://www.googletagmanager.com/ns.html?id=GTM-T3N838XB'
-              style={{ width: 0, height: 0, display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-        )}
         {children}
         {isProduction && <CookieConsent />}
       </body>
